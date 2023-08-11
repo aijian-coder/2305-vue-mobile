a1169da0e8b1255edc620ef1869d391a325ab8e1
Elvmx47 <elvmx47@outlook.com>
2023-08-11 16:07:57 +0800

    1111

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+import axios from "axios";
+
+const test = () => {
+  axios
+    .get("/api/gateway?k=8433057", {
+      headers: {
+        "X-Host": "mall.film-ticket.city.list",
+      },
+    })
+    .then((response) => {
+      console.log("response", response);
+    });
+};
+
+function test3000() {
+  axios.get("/custom/test").then((response) => {
+    console.log("response", response);
+  });
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <button @click="test">测试</button>
+    <button @click="test3000">测试 3000</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://m.maizuo.com",
+        // 修改目标服务请求头中 host 字段值为 target，避免 500
+        changeOrigin: true,
+        rewrite: (path) => path.replace("/api", ""),
+      },
+
+      "/custom": {
+        target: "http://127.0.0.1:3000",
+        // 修改目标服务请求头中 host 字段值为 target
+        changeOrigin: true,
+        rewrite: (path) => path.replace("/custom", ""),
+      },
+    },
+  },
+});
